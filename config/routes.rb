2c6a7d1716c91020b961dfdc8cf05011a2255467
Scott Le <scott.lee318@gmail.com>
2015-09-05 14:02:46 +0700

    setup foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :incoming_hooks
+
+  root 'incoming_hooks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
